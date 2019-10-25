9d93f2e533b0dcd98a06a1078850be704155521e
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-10-25 10:29:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magento2: Docker setup
+This repository automates the setup described in this tutorial by Shawn Abramson, with a few updates: https://www.magemodule.com/all-things-magento/magento-2-tutorials/docker-magento-2-development/
+
+## Resources
+* Docker image (web): https://dockerfile.readthedocs.io/en/latest/content/DockerImages/dockerfiles/php-apache-dev.html#docker-image-tags
---
+++ setup-docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# By default, the shop will be available under https://${projectname}.local when the setup is complete.
+
+projectName="$1"
+domainName="${projectName}.local"
+
+if [[ $# -ne 1 ]]; then
+echo "Usage: $0 project-name"
+    exit 1
+fi
+
+# Detect OS
+case `uname -s` in
+    Darwin) is_macos=yes ;;
+    *) is_macos=no ;;
+esac
+
+searchAndReplace() {
+    local original="$1"
+    local replacement="$2"
+    local file="$3"
+    if [ "$is_macos" = no ]; then
+        sed -i "s/$original/$replacement/g" $file
+    elif [ "$is_macos" = yes ]; then
+        sed -i '' "s/$original/$replacement/g" $file
+    fi
+}
+
+# Increase memory limits
+find $projectName -name '.htaccess' -exec sed -i '' s/756M/2048M/g {} + && \
+find $projectName -name '.htaccess' -exec sed -i '' s/768M/2048M/g {} + && \
+find $projectName -name '.user.ini' -exec sed -i '' s/756M/2048M/g {} + && \
+find $projectName -name '.user.ini' -exec sed -i '' s/768M/2048M/g {}
+
+searchAndReplace "/path/to/magento" "${PWD}/${projectName}" docker-compose.yml
+searchAndReplace "local.domain.com" "$domainName" docker-compose.yml
+
+echo "Adding ${domainName} to /etc/hosts..."
+echo "please enter your password:"
+sudo -- sh -c "echo '127.0.0.1 ${domainName}' >> /etc/hosts"
